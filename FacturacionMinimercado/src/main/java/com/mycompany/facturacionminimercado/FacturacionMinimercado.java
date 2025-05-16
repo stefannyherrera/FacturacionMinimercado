db97fb64614ffd5f1ddd13d828bc2679891f0f12
pablo32d <pablo.rodriguez3@udea.edu.co>
2025-05-15 21:28:46 -0500

    prueba

--- FacturacionMinimercado/src/main/java/com/mycompany/facturacionminimercado/FacturacionMinimercado.java
+++ FacturacionMinimercado/src/main/java/com/mycompany/facturacionminimercado/FacturacionMinimercado.java
@@ -11,6 +11,6 @@ package com.mycompany.facturacionminimercado;
 public class FacturacionMinimercado {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
